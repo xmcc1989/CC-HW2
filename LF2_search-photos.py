a738f5d460e9d00c5b1739dcb3f1a67ecb11048f
Mei Xue <43460009+xmcc1989@users.noreply.github.com>
2022-11-13 18:48:51 -0500

    initial commit

---
+++ LF2_search-photos.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[58]:
+
+
+#aws user
+#bot
+#access key: AKIAYNMA4JYZZAWISICO
+#secrete access key: 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
+
+
+# In[32]:
+
+
+import json
+import urllib.parse
+import boto3
+import logging
+import os
+import pprint
+import time
+import boto3
+import requests
+import inflect
+from datetime import datetime
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+def lambda_handler(event, context):
+    message = event['q']
+    # message = 'bridge and red'
+    userid = '123' #hard code user id
+    
+    #get slots from lex
+    slots = get_slots(userid, message)
+#     print(slots)
+    
+    if len(slots) == 0:
+        return {
+            'statusCode': '500',
+            'messages': {"code":0,
+                         "message": "No User Input."}
+            }
+    
+    #construct query string
+    query_string = construct_querystring(slots)
+    
+    #query elastic search
+    hits = query_es(query_string)
+#     print(hits)
+    
+    #construct reponse
+    return {
+            'statusCode': '200',
+            'messages': construct_response(hits)
+            }
+
+
+def get_slots(userid, message):
+    
+    client = boto3.client("lex-runtime")
+    response = client.post_text(
+        botName = 'SearchBot',
+        botAlias ='dev',
+        userId = userid, # to be changed
+        # sessionAttributes={
+        #     'string': 'string'
+        # },
+        # requestAttributes={
+        #     'string': 'string'
+        # },
+        inputText = message,
+        # activeContexts=[
+        #     {
+        #         'name': 'string',
+        #         'timeToLive': {
+        #             'timeToLiveInSeconds': 123,
+        #             'turnsToLive': 123
+        #         },
+        #         'parameters': {
+        #             'string': 'string'
+        #         }
+        #     },
+        # ]
+    )
+
+    return response['slots']
+
+def construct_querystring(slots):
+    p = inflect.engine()
+    
+    query_string = ''
+    for i in slots:
+#         print(slots[i])
+        if slots[i] is not None:
+            if i == 'tag_two':
+                query_string = query_string + ' OR '
+            if p.singular_noun(slots[i]) == False:
+                query_string = query_string + slots[i]
+            else:
+                query_string = query_string + p.singular_noun(slots[i])
+    
+    return query_string
+
+
+def query_es(query_string):
+    
+    region = 'us-east-1'
+    service = 'es'
+    host = 'https://search-photos-i6xlvas6hikk7cammtg5zr625e.us-east-1.es.amazonaws.com' 
+    index = 'photos'
+    typ = '_search'
+    url = host + '/' + index + '/' + typ
+    
+    # Put the user query into the query DSL for more accurate search results.
+    # Note that certain fields are boosted (^).
+    query = {
+        "size": 1000,
+        "query": {
+            "query_string": {
+                "query": query_string,
+                "fields": ["labels"]
+            }
+        }
+    }
+
+    # Elasticsearch 6.x requires an explicit Content-Type header
+    headers = { "Content-Type": "application/json" }
+
+    # Make the signed HTTP request. Hide user info on purpose!!!
+    response = requests.get(url, auth=('mx608', '!Xmcc891001'), data=json.dumps(query), headers=headers)
+    
+    return json.loads(response.text)['hits']['hits']
+
+def construct_response(hits):        
+    results=[]
+    
+    if len(hits) == 0:
+        return {"results" : []}
+    
+    for item in hits:
+        bucket = item['_source']['bucket']
+        key = item['_source']['objectKey']
+        labels = item['_source']['labels']
+        url = "https://%s.s3.amazonaws.com/%s" % (bucket, key)
+        res = {"url": url,
+               "labels": labels}
+        results.append(res)
+    
+    return {"results" : results}
+
+
+# In[33]:
+
+
+resp = lambda_handler({'q':'people'}, '')
+resp
+
+
+# In[83]:
+
+
+response = query_es('bridge')
+response.text
+
+
+# In[ ]:
+
+
+url = "https://s3-%s.amazonaws.com/%s/%s" % (location, bucket_name, key)
+
+
+# In[125]:
+
+
+s = get_slots('123','bridge and red')
+s
+
+
+# In[112]:
+
+
+type(s['tag_two'])
+
+
+# In[1]:
+
+
+pip install inflect 
+
+
+# In[18]:
+
+
+
+
+p = inflect.engine()
+
+s = p.singular_noun('bridge')
+s
+
+# ss = p.plural('leaf')
+# ss
+
+
+# In[31]:
+
+
+p.singular_noun('people')
+
+
+# In[ ]:
+
+
+
+
